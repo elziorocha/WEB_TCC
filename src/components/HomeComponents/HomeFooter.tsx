a6700f83598b9ffdaf378c66eb8f0a688b5f6ed1
elziorocha <rochasenzo.04@gmail.com>
2025-07-31 00:53:22 -0300

    feat: ajustes na formulação dos layouts com publicação de branch;
    fix: ajustes no css global, com adição de variáveis root e fonte padrão da aplicação;
    
    feat: adição de protótipo de menubar mobile

--- src/components/HomeComponents/HomeFooter.tsx
+++ src/components/HomeComponents/HomeFooter.tsx
@@ -1,5 +1,13 @@
 export const HomeFooter = () => {
-  return <main className="py-4 px-8 bg-primary">HomeFooter</main>;
+  return (
+    <main className="py-4 px-5 bg-secondary">
+      <h2 className="text-whiteText text-center text-xs sm:text-sm md:text-lg">
+        Rua Sorocaba, 277 - Bonsucesso - Cep: 85055-090
+        <br />
+        Guarapuava, Paraná | Fone: (42) 3035-3388
+      </h2>
+    </main>
+  );
 };
 
 export default HomeFooter;
--- src/components/HomeComponents/HomeHeader.tsx
+++ src/components/HomeComponents/HomeHeader.tsx
@@ -1,10 +1,35 @@
-import logo from '../..//assets/logo1.png'
+import { Menu } from "lucide-react";
+import logo from "../..//assets/logo1.png";
+import { useState } from "react";
 
 export const HomeHeader = () => {
-  return <main className="py-4 px-8 bg-primary">
-    {/* meu deus não tem uma imagem que preste pra coloca */}
-    <img src={logo} alt="logo da pérola do oeste" className=''/>
-  </main>;
+  const [menuAtivo, setMenuAtivo] = useState(false);
+
+  return (
+    <main className="py-4 px-5 bg-secondary text-whiteText flex items-center justify-between">
+      <img src={logo} alt="logo da pérola do oeste" className="" />
+
+      <Menu
+        onClick={() => setMenuAtivo(!menuAtivo)}
+        className={`size-10 p-1 rounded-lg block sm:block md:hidden z-50
+          ${menuAtivo ? "bg-tertiary" : "bg-secondary"}`}
+      />
+
+      <section
+        className={`absolute top-0 left-0 w-full h-full bg-secondary
+          ${menuAtivo ? "opacity-100" : "opacity-0"}`}
+        style={{ transition: "transform 0.3s ease, opacity 0.3s ease" }}
+      >
+        <ul className="flex flex-col sm:flex-col md:flex-row">
+          <li>rota 1</li>
+          <li>rota 2</li>
+          <li>rota 3</li>
+          <li>rota 4</li>
+          <li>rota 5</li>
+        </ul>
+      </section>
+    </main>
+  );
 };
 
 export default HomeHeader;
