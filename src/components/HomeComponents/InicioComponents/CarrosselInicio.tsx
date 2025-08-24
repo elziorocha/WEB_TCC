8d251eb87300dbc8e25d74aa738a17ebc1ebec56
elziorocha <rochasenzo.04@gmail.com>
2025-08-24 19:02:12 -0300

    fix: ajuste nos objetos de imagens

--- src/components/HomeComponents/InicioComponents/CarrosselInicio.tsx
+++ src/components/HomeComponents/InicioComponents/CarrosselInicio.tsx
@@ -6,29 +6,25 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-
-import banner1 from "../../../assets/CarouselImages/banner_1.jpg";
-import banner2 from "../../../assets/CarouselImages/banner_2.jpg";
-import banner3 from "../../../assets/CarouselImages/banner_3.jpg";
-import banner4 from "../../../assets/CarouselImages/banner_4.jpg";
-import banner5 from "../../../assets/CarouselImages/banner_5.png";
-import banner6 from "../../../assets/CarouselImages/banner_6.jpg";
+import { inicioBanners } from "@/utils/objetosExportaveis";
 
 const CarrosselInicio = () => {
-  const imagens = [banner1, banner2, banner3, banner4, banner5, banner6];
-
   return (
     <main>
       <Carousel className="w-full">
         <CarouselContent>
-          {imagens.map((imagem, index) => (
+          {inicioBanners.map((imagem, index) => (
             <CarouselItem key={index}>
               <Card className="relative border-none shadow-md rounded-none p-0">
                 <CardContent className="flex items-center justify-center p-0">
-                  <img src={imagem} className="w-full h-full object-contain" />
-                  <div className="absolute inset-0 flex items-center justify-between p-4 pointer-events-none">
-                    <CarouselPrevious className="pointer-events-auto relative left-0 top-0 translate-y-0 bg-white/80 hover:bg-white shadow-md border-none cursor-pointer" />
-                    <CarouselNext className="pointer-events-auto relative right-0 top-0 translate-y-0 bg-white/80 hover:bg-white shadow-md border-none cursor-pointer" />
+                  <img
+                    src={imagem.src}
+                    alt={imagem.alt}
+                    className="w-full h-full object-contain"
+                  />
+                  <div className="absolute inset-0 flex items-center justify-between p-3 pointer-events-none">
+                    <CarouselPrevious className="pointer-events-auto relative left-0 top-0 translate-y-0 bg-white/40 hover:bg-white shadow-md border-none cursor-pointer" />
+                    <CarouselNext className="pointer-events-auto relative right-0 top-0 translate-y-0 bg-white/40 hover:bg-white shadow-md border-none cursor-pointer" />
                   </div>
                 </CardContent>
               </Card>
--- src/utils/objetosExportaveis.ts
+++ src/utils/objetosExportaveis.ts
@@ -55,3 +55,37 @@ export const navUsuarios = [
     label: "Área do Colaborador",
   },
 ];
+
+import banner1 from "../assets/CarouselImages/banner_1.jpg";
+import banner2 from "../assets/CarouselImages/banner_2.jpg";
+import banner3 from "../assets/CarouselImages/banner_3.jpg";
+import banner4 from "../assets/CarouselImages/banner_4.jpg";
+import banner5 from "../assets/CarouselImages/banner_5.png";
+import banner6 from "../assets/CarouselImages/banner_6.jpg";
+
+export const inicioBanners = [
+  {
+    src: banner1,
+    alt: "Imagem sobre cuidado nas faixas amarelas",
+  },
+  {
+    src: banner2,
+    alt: "Imagem sobre preservar o patrimônio coletivo",
+  },
+  {
+    src: banner3,
+    alt: "Imagem sobre incentivo ao transporte público",
+  },
+  {
+    src: banner4,
+    alt: "Imagem sobre o portal do aluno",
+  },
+  {
+    src: banner5,
+    alt: "Imagem sobre bons modos com o público",
+  },
+  {
+    src: banner6,
+    alt: "Imagem sobre o cartão VEM",
+  },
+];
