90d12fed527cc06fe501df2b9a5306b00a782901
elziorocha <rochasenzo.04@gmail.com>
2025-09-17 01:17:59 -0300

    feat: utilização de dotenv para boas práticas de segurança;

--- src/services/instanciaApi.ts
+++ src/services/instanciaApi.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const instanciaAPI = axios.create({
-  baseURL: 'http://localhost:3000/api/',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
 
