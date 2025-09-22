fc1835f39c84fa6065c9b305b57c75a1986d0366
elziorocha <rochasenzo.04@gmail.com>
2025-09-22 20:23:33 -0300

    feat: adição de menu de navegação para desktop;
    fix: ajuste na scrollbar do website;

--- src/components/HomeComponents/HomeHeader.tsx
+++ src/components/HomeComponents/HomeHeader.tsx
@@ -6,7 +6,7 @@ export const HomeHeader = () => {
   return (
     <main className="bg-secondary text-whiteText sticky top-0 z-50 flex items-center justify-between py-4 pr-4 pl-1">
       <Link to="/">
-        <img src={logo} alt="logo da pérola do oeste" className="" />
+        <img src={logo} alt="logo da pérola do oeste" />
       </Link>
 
       <MenuComponent />
--- src/components/componentesUI/MenuComponent.tsx
+++ src/components/componentesUI/MenuComponent.tsx
@@ -15,15 +15,46 @@ import {
 
 export default function MenuComponent() {
   const [menuAtivo, setMenuAtivo] = useState(false);
-
   const fecharMenu = () => setMenuAtivo(false);
 
-  return (
+  const MenuDesktop = () => (
+    <main className="hidden items-center gap-6 md:flex">
+      <section className="flex items-center gap-4">
+        {navHome.map((item) => (
+          <Link
+            key={item.key}
+            to={item.key}
+            className="hover:bg-quarter flex items-center gap-2 rounded-lg px-3 py-2 transition-colors"
+          >
+            <item.icon size={20} />
+            <span className="text-white">{item.label}</span>
+          </Link>
+        ))}
+      </section>
+
+      <hr className="h-10 border border-zinc-300" />
+
+      <section className="flex items-center gap-4">
+        {navUsuarios.map((item) => (
+          <Link
+            key={item.key}
+            to={item.key}
+            className="hover:bg-quarter flex items-center gap-2 rounded-lg px-3 py-2 transition-colors"
+          >
+            <item.icon size={20} />
+            <span className="text-white">{item.label}</span>
+          </Link>
+        ))}
+      </section>
+    </main>
+  );
+
+  const MenuMobile = () => (
     <Sheet open={menuAtivo} onOpenChange={setMenuAtivo}>
       <SheetTrigger>
         <Menu
           onClick={() => setMenuAtivo(!menuAtivo)}
-          className="bg-tertiary z-50 block size-10 rounded-lg p-1 sm:block md:hidden"
+          className="bg-tertiary z-50 block size-10 rounded-lg p-1 md:hidden"
         />
       </SheetTrigger>
       <SheetContent
@@ -64,5 +95,12 @@ export default function MenuComponent() {
         </section>
       </SheetContent>
     </Sheet>
+  );
+
+  return (
+    <main className="flex w-full items-center justify-center">
+      <MenuDesktop />
+      <MenuMobile />
+    </main>
   );
 }
