8025bbcdf86605bcefd747408519496f05009ade
elziorocha <rochasenzo.04@gmail.com>
2025-09-25 02:17:20 -0300

    fix: ajuste de texto;

--- src/pages/HomePage/Tarifas.tsx
+++ src/pages/HomePage/Tarifas.tsx
@@ -4,7 +4,7 @@ import { TarifasEducardInfo } from '@/components/HomeComponents/TarifasEducardIn
 export const Tarifas = () => {
   return (
     <main className="flex flex-col gap-4 p-4">
-      <h2 className="text-tertiary my-2 text-center text-xl font-semibold sm:text-2xl">
+      <h2 className="text-tertiary my-2 text-center text-xl font-bold sm:text-2xl">
         Quais os custos{' '}
         <span className="font-semibold text-green-600 italic">tarifários</span>{' '}
         dos nossos serviços?
@@ -14,9 +14,9 @@ export const Tarifas = () => {
 
       <hr className="w-11/12 self-center border-zinc-400" />
 
-      <article className="flex flex-col gap-4">
-        <h2 className="text-tertiary text-center text-xl font-semibold sm:text-2xl">
-          Como saber qual cartão devo utilizar?
+      <article className="mt-2 flex flex-col gap-4">
+        <h2 className="text-tertiary text-center text-xl font-bold sm:text-2xl">
+          O que mais posso saber?
         </h2>
       </article>
 
