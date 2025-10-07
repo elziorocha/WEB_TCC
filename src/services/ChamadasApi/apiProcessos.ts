a86c95307512075f2f8bea39b3c03ed8850c6fc7
elziorocha <rochasenzo.04@gmail.com>
2025-10-06 22:23:17 -0300

    fix;

--- src/services/ChamadasApi/apiProcessos.ts
+++ src/services/ChamadasApi/apiProcessos.ts
@@ -1,24 +1,24 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { apiError } from '../apiError';
 import {
   getAlunoProcessos,
   iniciarAlunoProcesso,
-  postAlunoProcessos as postAlunoProcessosAPI,
+  postAlunoProcessos,
 } from '../api';
 import toast from 'react-hot-toast';
 
-export const chamadaAlunoProcessos = () => {
+export function alunoProcessosData() {
   const [alunoProcessos, setAlunoProcessos] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchAlunoProcessos = async () => {
       try {
-        const dados = await getAlunoProcessos();
-        setAlunoProcessos(dados);
-      } catch (err) {
+        const dadosAlunoProcessos = await getAlunoProcessos();
+        setAlunoProcessos(dadosAlunoProcessos);
+      } catch (err: unknown) {
         console.error(err);
-        apiError(err, 'Erro ao carregar processos do aluno');
+        apiError('Erro ao carregar dados de processos do aluno');
       } finally {
         setLoading(false);
       }
@@ -28,27 +28,27 @@ export const chamadaAlunoProcessos = () => {
   }, []);
 
   return { alunoProcessos, loading };
-};
+}
 
-export const chamadaCriarProcesso = () => {
+export function criarAlunoProcesso() {
   const [loading, setLoading] = useState(false);
 
   const criarProcesso = async (formData: FormData) => {
     setLoading(true);
+
     try {
       const processo = await iniciarAlunoProcesso();
 
-      await postAlunoProcessosAPI(formData);
-
       if (!processo.existente) {
+        await postAlunoProcessos(formData);
         toast.success('Processo iniciado e documentos enviados com sucesso!');
       } else {
+        await postAlunoProcessos(formData);
         toast.success('Documentos atualizados no processo existente!');
       }
 
       return processo;
     } catch (err: any) {
-      console.error(err);
       apiError(err, 'Erro ao criar processo.');
     } finally {
       setLoading(false);
@@ -56,4 +56,4 @@ export const chamadaCriarProcesso = () => {
   };
 
   return { criarProcesso, loading };
-};
+}
